f8df9e162bae89115f4d0f694990dda59a36d965
lop <61240663+loplxl@users.noreply.github.com>
2026-09-12 00:34:26 +0000

    Log missing libevdev installation
    Add check for libevdev installation and log if missing

--- scripts/gd.sh
+++ scripts/gd.sh
@@ -15,6 +15,9 @@ export PROTON_USE_NTSYNC=1
 export WINEDLLOVERRIDES="xinput1_4=n,b"
 # Proton 11 fix
 export LD_PRELOAD=$(ldconfig -p | awk '/libevdev\.so/{print $NF; exit}')
-
+if [[ -z "$LD_PRELOAD" ]]; then
+	mkdir ~/gd-low-linux-latency-logs
+	echo "No libevdev file found, you should install libevdev" >> ~/gd-low-linux-latency-logs/log.txt
+fi
 # Launch the game
 exec "$@"
